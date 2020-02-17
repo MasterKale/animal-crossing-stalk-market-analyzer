7e139e40feedf51c91e0f2968fc3b64708c9a160
Matthew Miller <matthew@millerti.me>
2020-02-16 22:13:53 -0800

    Add new tests using sample data from Reddit

--- turnips.test.js
+++ turnips.test.js
@@ -21,6 +21,15 @@ const spikeSmallPartial = [73, 72, 69, 50, 49, 80, 92, 103, 107];
 // Could be a big or small spike (both have at least three consecutive increases)
 const spikeIndeterminate = [73, 72, 69, 80, 92, 112];
 
+/**
+ * From this Google Sheet on Reddit
+ * https://docs.google.com/spreadsheets/d/1qNojSBK8mpQRkqXjnOJ75Mk7ZNiG3DRIZHwFQsk6kFY/edit
+ */
+const week13082018 = [73, 69, 65, 60, 57, 111, 112, 190, 192, 167, 78, 74];
+const week20082018 = [145, 128, 140, 147, 115, 103, 66, 61, 105, 80, 61, 50];
+const week27082018 = [69, 66, 63, 59, 56, 52, 48, 101, 97, 170, 187, 173];
+const week03092018 = [118, 121, 112, 107, 100, 61, 56, 50, 48, 61, 51, 49];
+
 test('Decreasing', () => {
   expect(determinePattern(decreasing)).toEqual(PATTERN.DECREASING);
 });
@@ -49,3 +58,18 @@ test('Spike - (Partial, Early)', () => {
   expect(determinePattern(spikePartialEarly)).toEqual(PATTERN.SPIKE_BIG);
 });
 
+test('week13082018', () => {
+  expect(determinePattern(week13082018)).toEqual(PATTERN.SPIKE_SMALL);
+});
+
+test('week20082018', () => {
+  expect(determinePattern(week20082018)).toEqual(PATTERN.RANDOM);
+});
+
+test('week27082018', () => {
+  expect(determinePattern(week27082018)).toEqual(PATTERN.RANDOM);
+});
+
+test('week03092018', () => {
+  expect(determinePattern(week03092018)).toEqual(PATTERN.RANDOM);
+});
